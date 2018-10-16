fa2b144ba4ca2d104ac4dc776e26497a85479c71
veerahentinen@hotmail.com <Styx1369>
2018-10-16 14:47:43 +0300

    yay

--- condition-tasks/IfTask3/IfTask3/Program.cs
+++ condition-tasks/IfTask3/IfTask3/Program.cs
@@ -7,7 +7,10 @@ namespace IfTask3
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma selvittää onko luku negatiivinen vai positiivinen sekä parillinen vai pariton.");
-            Console.WriteLine("Syötä numero");
+            Console.Write("Syötä numero: ");
+            string userInput = Console.ReadLine();
+            int number = 0;
+
         }
     }
 }
